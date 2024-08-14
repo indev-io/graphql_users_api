9558996c78a52039cc4274a03d5589c4b6c831a7
ImmortalCount <devnaut@gmail.com>
2024-08-14 11:10:16 -0700

    all queries, mutations and subscriptions working

--- lib/graphql_users_api_web/schema.ex
+++ lib/graphql_users_api_web/schema.ex
@@ -164,11 +164,23 @@ defmodule GraphqlUsersApiWeb.Schema do
   subscription do
     field :created_user, :user do
       trigger :create_user, topic: fn _ ->
-        "new user"
+        "new_user"
       end
       config fn _, _ ->
         {:ok, topic: "new_user"}
       end
     end
+
+    field :updated_user_preferences, :user do
+    arg :id, non_null(:id)
+      trigger :update_user_preferences, topic: fn user ->
+        user.id
+      end
+      config fn args, _ ->
+        {:ok, topic: args.id}
+      end
+    end
   end
+
+
 end
