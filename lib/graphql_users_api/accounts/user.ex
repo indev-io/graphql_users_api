172a7269e774d9f919b44e307ae82dbd802c35b9
ImmortalCount <devnaut@gmail.com>
2024-09-13 10:55:45 -0700

    comments added

--- lib/graphql_users_api/accounts/user.ex
+++ lib/graphql_users_api/accounts/user.ex
@@ -20,29 +20,27 @@ defmodule GraphqlUsersApi.Accounts.User do
   @doc false
   def changeset(user, attrs \\ %{}) do
     user
-    GraphqlUsersApi.Repo.preload([:preferences])
     |> cast(attrs, @available_fields)
     |> validate_required(@available_fields)
     |> cast_assoc(:preferences)
   end
 
-  def by_likes_faxes(query \\ GraphqlUsersApi.Accounts.User, bool) do
+  def by_likes_faxes(query, bool) do
     query = maybe_join_preferences(query)
     from [u, p] in query, where: p.likes_faxes == ^bool
   end
 
-  def by_likes_emails(query \\ GraphqlUsersApi.Accounts.User, bool) do
+  def by_likes_emails(query , bool) do
     query = maybe_join_preferences(query)
     from [u, p] in query, where: p.likes_emails == ^bool
   end
 
-  def by_likes_phone_calls(query \\ GraphqlUsersApi.Accounts.User, bool) do
+  def by_likes_phone_calls(query, bool) do
     query = maybe_join_preferences(query)
     from [u, p] in query, where: p.likes_phone_calls == ^bool
   end
 
   def by_name(query \\ GraphqlUsersApi.Accounts.User, val) do
-    IO.inspect(query)
     where(query, [u], u.name == ^val)
   end
 
@@ -51,20 +49,28 @@ defmodule GraphqlUsersApi.Accounts.User do
     where(query, [u], u.email == ^val)
   end
 
+
   # def join_preferences(query \\ GraphqlUsersApi.Accounts.User) do
+  #   query = preload(query, :preferences)
   #   join(query, :inner, [u], p in assoc(u, :preferences), as: :preferences)
+  # end
+
+  #running this function returns unknown bind name :preferences in query when testing in graphiql playground,
+  #yet works when testing through IEX
+
+  # def by_likes_phone_calls(query \\ join_preferences(), bool) do
+  #   where(query, [u, preferences: p], p.likes_phone_calls == ^bool)
   # end
 
   defp maybe_join_preferences(query) do
     cond do
+      #guard against User used in accumulator
       !is_map(query) ->
-      from u in query, join: p in assoc(u, :preferences)
+      from u in query, preload: :preferences, join: p in assoc(u, :preferences)
       !Keyword.has_key?(query.assocs, :preferences) ->
-      from u in query, join: p in assoc(u, :preferences)
+      from u in query, preload: :preferences, join: p in assoc(u, :preferences)
       true -> query
     end
   end
-
-
 
 end
